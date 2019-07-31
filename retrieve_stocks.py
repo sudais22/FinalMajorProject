48d497c6eef4c0cdc6edf80e919054f3d605c2c0
sudais22 <48554185+sudais22@users.noreply.github.com>
2019-07-31 09:21:59 +0800

    Update retrieve_stocks.py

--- retrieve_stocks.py
+++ retrieve_stocks.py
@@ -1,6 +1,6 @@
 import psycopg2 as p
 
-def getStockData():
+def getStockData(id):
     connection = 0
     try:
         connection = p.connect(user="postgres",
@@ -16,8 +16,24 @@ def getStockData():
         print("Error while connecting to PostgreSQL: ", error, "\n")
     finally:
         if(connection):
-            #retrieveStocks = "SELECT row_to_json(row(day, closing_price, volume)) FROM stocktrade order by day ASC"
-            retrieveStocks = "select row_to_json(t) from ( select (select array_to_json(array_agg(row_to_json(d))) from (select day, closing_price, volume from stocktrade_2 order by day ASC) d ) as info) t "
+            if id == 1:
+                retrieveStocks = "select row_to_json(t) from ( select (select array_to_json(array_agg(row_to_json(d))) from (select day, closing_price, volume from stocktrade_2 order by day ASC) d ) as info) t "
+            elif id == 2:
+                retrieveStocks = "select row_to_json(t) from ( select (select array_to_json(array_agg(row_to_json(d))) from (select day, closing_price, volume from stocktrade_4 order by day ASC) d ) as info) t "
+            elif id == 3:
+                retrieveStocks = "select row_to_json(t) from ( select (select array_to_json(array_agg(row_to_json(d))) from (select day, closing_price, volume from stocktrade_6 order by day ASC) d ) as info) t "
+            elif id == 4:
+                retrieveStocks = "select row_to_json(t) from ( select (select array_to_json(array_agg(row_to_json(d))) from (select day, closing_price, volume from stocktrade_7 order by day ASC) d ) as info) t "
+            elif id == 5:
+                retrieveStocks = "select row_to_json(t) from ( select (select array_to_json(array_agg(row_to_json(d))) from (select day, closing_price, volume from stocktrade_8 order by day ASC) d ) as info) t "
+            elif id == 6:
+                retrieveStocks = "select row_to_json(t) from ( select (select array_to_json(array_agg(row_to_json(d))) from (select day, closing_price, volume from stocktrade_10 order by day ASC) d ) as info) t "
+            elif id == 7:
+                retrieveStocks = "select row_to_json(t) from ( select (select array_to_json(array_agg(row_to_json(d))) from (select day, closing_price, volume from stocktrade_11 order by day ASC) d ) as info) t "
+            elif id == 8:
+                retrieveStocks = "select row_to_json(t) from ( select (select array_to_json(array_agg(row_to_json(d))) from (select day, closing_price, volume from stocktrade_12 order by day ASC) d ) as info) t "
+            else:
+                retrieveStocks = "select row_to_json(t) from ( select (select array_to_json(array_agg(row_to_json(d))) from (select day, closing_price, volume from stocktrade_2 order by day ASC) d ) as info) t "
             cursor.execute(retrieveStocks)
             result = cursor.fetchall()
             return result
@@ -26,5 +42,3 @@ def getStockData():
         cursor.close()
         connection.close()
         print("Disconnected from postgreSQL")
-
-        
